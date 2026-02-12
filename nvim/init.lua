ae3266e59dafc9d778d333f2eb24b58b10e1b6b2
lescarpi <leandroscarpigranelli@gmail.com>
2026-02-12 01:46:37 -0300

    first dotfiles

---
+++ nvim/init.lua
@@ -0,0 +1,40 @@
+-- lazy.nvim
+require("config.lazy")
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Scroll
+vim.opt.scrolloff = 999
+
+-- Save
+vim.keymap.set("n", "<C-s>", ":w<CR>")
+vim.keymap.set("i", "<C-s>", "<C-o>:w<CR>")
+
+-- Linhas
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Identação
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.expandtab = true
+
+-- Color Scheme
+vim.opt.termguicolors = true
+
+-- PageUp and PageDown
+vim.keymap.set("i", "<PageDown>", "<Nop>")
+vim.keymap.set("i", "<PageUp>", "<Nop>")
+vim.keymap.set("n", "<PageDown>", "<C-f>zz")
+vim.keymap.set("n", "<PageUp>", "<C-b>zz")
+
+-- Rust
+vim.api.nvim_create_autocmd("Filetype", {
+        pattern = "rust",
+        callback = function()
+                vim.opt_local.tabstop = 8
+                vim.opt_local.shiftwidth = 8
+                vim.opt_local.expandtab = true
+        end
+})
